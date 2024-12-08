a524de63e19f0d62bc898f5dfe47c89121da7b1d
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 20:17:36 +0200

    fixing task7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -10,6 +10,6 @@ Return: return_description
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple of a string and a number"""
-    return (k, float(v ** 2))
+    return (k, pow(v, 2))
