f162e8438b577a5349c60f7cb19bf6269c4cc2dd
3zzazakl <vanzekovichgr@gmail.com>
2024-12-09 00:03:28 +0200

    fixing styles

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """wait_n
     Keyword arguments:
