dbfdb70c1a6b986d3cf0922c60b771b0d1d74aeb
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 19:11:49 +0200

    checking task0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+def add(a: float, b: float) -> float:
+    """returns sum of two floats"""
+    return a + b
