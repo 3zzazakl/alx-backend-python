dbcb4b50aae69f7110d30aa6e6114d8903e3971a
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 19:24:12 +0200

    checking task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+from typing import List
+
+
+def sum_list(a: List[float]) -> float:
+    """returns sum of a list of floats"""
+    return sum(a)
