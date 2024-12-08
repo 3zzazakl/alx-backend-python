3fa92cfa0412627d2d4c22f0ea82e7a07a8ef463
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 19:19:22 +0200

    checking task1

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+import math
+
+
+def floor(a: float) -> float:
+    """returns floor of a float"""
+    return math.floor(a)
