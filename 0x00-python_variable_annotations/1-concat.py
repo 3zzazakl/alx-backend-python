5b2612017c4d51aace1fc117edfc0cbccb9eec95
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 19:17:38 +0200

    checking task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """returns concatenation of two strings"""
+    return a + b
