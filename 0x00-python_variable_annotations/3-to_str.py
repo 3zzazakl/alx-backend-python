f19770fbaf59aa4ff2634f682fc33a7305d739f2
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 19:20:50 +0200

    checking task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+def to_str(a: float) -> str:
+    """returns string representation of a float"""
+    return str(a)
