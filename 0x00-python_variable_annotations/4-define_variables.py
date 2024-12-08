821b39f101ec3bfaf3ed3555c0c679b1ac669151
3zzazakl <vanzekovichgr@gmail.com>
2024-12-08 19:22:39 +0200

    checking task4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "ALX"
