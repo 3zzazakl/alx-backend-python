444b2b0a5cd59ff71a76db2aca66fe5490e81a8e
3zzazakl <vanzekovichgr@gmail.com>
2024-12-10 08:49:03 +0200

    checking task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,11 @@ Keyword arguments:
 argument -- description
 Return: return_description
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """sumary_line
 
     Keyword arguments:
