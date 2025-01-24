ab97b0762808dc27af10f1f6d1e8770e037ea8d4
3zzazakl <vanzekovichgr@gmail.com>
2025-01-24 05:35:21 +0200

    setting files

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """summary_line
-    
+
     Keyword arguments:
     argument -- description
     Return: return_description
