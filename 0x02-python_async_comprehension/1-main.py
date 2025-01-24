f2460e2d4eb80dccabdb4a1084a12e2349ad3264
3zzazakl <vanzekovichgr@gmail.com>
2025-01-24 05:30:56 +0200

    setting files

--- 0x02-python_async_comprehension/1-main.py
+++ 0x02-python_async_comprehension/1-main.py
@@ -6,6 +6,13 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def main():
+    """summary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
     print(await async_comprehension())
 
 asyncio.run(main())
