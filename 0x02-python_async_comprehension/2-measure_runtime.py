552360d742f0fd5b2cee9e9fcdd03ff55a2366f5
3zzazakl <vanzekovichgr@gmail.com>
2025-01-24 05:53:00 +0200

    checking task2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -18,11 +18,7 @@ async def measure_runtime() -> float:
     Return: return_description
     """
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*tasks)
     end_time = time.time()
     return end_time - start
